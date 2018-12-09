f960bd1ccabe3d1e653789b146334abf3679a8ae
SinghMukeshK <mukesh06081990@gmail.com>
2018-12-09 12:25:26 +0530

    commit

--- src/layouts/Dashboard/Dashboard.jsx
+++ src/layouts/Dashboard/Dashboard.jsx
@@ -3,8 +3,8 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Switch, Route, Redirect } from "react-router-dom";
 // creates a beautiful scrollbar
-import PerfectScrollbar from "perfect-scrollbar";
-import "perfect-scrollbar/css/perfect-scrollbar.css";
+//import PerfectScrollbar from "perfect-scrollbar";
+//import "perfect-scrollbar/css/perfect-scrollbar.css";
 // @material-ui/core components
 import withStyles from "@material-ui/core/styles/withStyles";
 // core components
@@ -48,9 +48,9 @@ class App extends React.Component {
     }
   }
   componentDidMount() {
-    if (navigator.platform.indexOf("Win") > -1) {
-      const ps = new PerfectScrollbar(this.refs.mainPanel);
-    }
+    //if (navigator.platform.indexOf("Win") > -1) {
+     // const ps = new PerfectScrollbar(this.refs.mainPanel);
+    //}
     window.addEventListener("resize", this.resizeFunction);
   }
   componentDidUpdate(e) {
